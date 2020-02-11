e4cbc36205c3f44f1c26b83323ebfdca22d58da2
luzarce07 <luzarce7@gmail.com>
2020-02-11 11:23:51 -0700

    movement with velocity

--- Assets/Scripts/GameplaySystem.cs
+++ Assets/Scripts/GameplaySystem.cs
@@ -4,49 +4,86 @@ namespace Platform2DUtils.GameplaySystem
     
     public class GameplaySystem
     {
-        /// <summary>
-        /// Returns a Vector2 with Horizontal and Vertical axis
-        /// </summary>
+        ///<summary>
+        /// Returns a Vector2 with Horizontal and Vertical axes.
+        ///</summary>
         public static Vector2 Axis
         {
-            get => new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+            get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         }
-         /// <summary>
-        /// Moves player in Horizontal axis with keyboard inputs using cpu speed.
-        /// </summary>
-        /// <param name="t">Transform component of the player</param>
-        /// <param name="moveSpeed">The coefficient of speed</param>
 
+        ///<summary>
+        /// Moves player in Horizontal axis with keyboard inputs.
+        ///</summary>
+        ///<param name="t">Transform component of the player</param>
+        ///<param name="moveSpeed">The coeficient of speed</param>
         public static void TMovement(Transform t, float moveSpeed)
         {
-            t.Translate(Vector2.right * moveSpeed * Axis.x);
-        }
-         /// <summary>
-        /// Moves player in Horizontal axis with keyboard inputs using time.
-        /// </summary>
-        /// <param name="t">Transform component of the player</param>
-        /// <param name="moveSpeed">The coefficient of speed</param>
-
-        public static void TMovementDelta(Transform t, float moveSpeed)
-        {
-            t.Translate(Vector2.right * moveSpeed * Time.deltaTime * Axis.x);
+            t.Translate(Vector2.right * Axis.x * moveSpeed);
         }
 
-         /// <summary>
-        /// Moves player in Horizontal axis with keyboard inputs using time.
-        /// </summary>
-        /// <param name="rb2D">Rigidbody2D component of the player</param>
-        /// <param name="moveSpeed">The coefficient of speed</param>
-
-        public static void MovementAddForce(Rigidbody2D rb2D, float moveSpeed)
+        ///<summary>
+        /// Moves player in Horizontal axis with keyboard inputs uisng force.
+        ///</summary>
+        ///<param name="rb2D">Rigidbody2D component of the player</param>
+        ///<param name="moveSpeed">The coeficient of speed</param>
+        ///<param name="maxVel">Maximum velocity of rigidbody on x component</param>
+        public static void MovementAddForce(Rigidbody2D rb2D, float moveSpeed, float maxVel)
         {
             rb2D.AddForce(Vector2.right * moveSpeed * Axis.x, ForceMode2D.Impulse);
+            float velXClamp = Mathf.Clamp(rb2D.velocity.x, -maxVel, maxVel);
+            rb2D.velocity = new Vector2(velXClamp, rb2D.velocity.y);
+            if(Axis.x == 0)
+            {
+                rb2D.velocity = new Vector2(0f, rb2D.velocity.y);
+            }
         }
 
-        /// <summary>
-        /// Returns if jump button was buttondown
-        /// </summary>
+        ///<summary>
+        /// Moves player in Horizontal axis with keyboard inputs uisng force.
+        ///</summary>
+        ///<param name="rb2D">Rigidbody2D component of the player</param>
+        ///<param name="moveSpeed">The coeficient of speed</param>
+        ///<param name="maxVel">Maximum velocity of rigidbody on x component</param>
+        ///<param name="grounding">Detects if im touching groundf layer</param>
+        public static void MovementAddForce(Rigidbody2D rb2D, float moveSpeed, float maxVel, bool grounding)
+        {
+            rb2D.AddForce(Vector2.right * moveSpeed * Axis.x, ForceMode2D.Impulse);
+            float velXClamp = Mathf.Clamp(rb2D.velocity.x, -maxVel, maxVel);
+            rb2D.velocity = new Vector2(velXClamp, rb2D.velocity.y);
 
+            if (Axis.x == 0 && grounding)
+            {
+                rb2D.velocity = new Vector2(0f, rb2D.velocity.y);
+            }
+        }
+
+        ///<summary>
+        /// Moves player in Horizontal axis with keyboard inputs using velocity.
+        ///</summary>
+        ///<param name="rb2D">Rigidbody2D component of the player</param>
+        ///<param name="moveSpeed">The coeficient of speed</param>
+        ///<param name="maxVel">Maximum velocity of rigidbody on x component</param>
+        public static void MovementVelocity(Rigidbody2D rb2D, float moveSpeed, float maxVel)
+        {
+            rb2D.velocity = new Vector2(Axis.x * moveSpeed, rb2D.velocity.y);
+            Vector2 clampedVelocity = Vector2.ClampMagnitude(rb2D.velocity, maxVel);
+            rb2D.velocity = new Vector2(clampedVelocity.x, rb2D.velocity.y);
+        }
+
+        ///<summary>
+        /// Moves player in Horizontal axis with keyboard inputs and multiplied by delta time.
+        ///</summary>
+        ///<param name="t">Transform component of the player</param>
+        ///<param name="moveSpeed">The coeficient of speed</param>
+        public static void TMovementDelta(Transform t, float moveSpeed)
+        {
+            t.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
+        }
+
+        ///<summary>
+        /// Returns if jump button was buttondown.
+        ///</summary>
         public static bool JumpBtn
         {
             get => Input.GetButtonDown("Jump");
@@ -54,12 +91,8 @@ namespace Platform2DUtils.GameplaySystem
 
         public static void Jump(Rigidbody2D rb2D, float jumpForce)
         {
-            
-            rb2D.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-
-
-
     }
 
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,58 +9,43 @@ public class Player : Character2D
     [SerializeField]
     float maxVel;
 
-      void Update()
-    {
-        //GameplaySystem.TMovementDelta(this.transform,moveSpeed);
-        
-    }
-
     void FixedUpdate()
     {
-
         if(GameplaySystem.JumpBtn)
         {
-
-            //Debug.Log("im working");
-            
             if(Grounding)
             {
                 anim.SetTrigger("jump");
-                GameplaySystem.Jump(rb2D,jumpForce);
+                GameplaySystem.Jump(rb2D, jumpForce);
             }
-
         }
-
         anim.SetBool("grounding", Grounding);
 
-        GameplaySystem.MovementAddForce(rb2D, moveSpeed);
-        float velXClamp = Mathf.Clamp(rb2D.velocity.x, -maxVel, maxVel);
-        rb2D.velocity = new Vector2(velXClamp, rb2D.velocity.y);
-        
+        //GameplaySystem.MovementAddForce(rb2D, moveSpeed, maxVel, Grounding);
+        GameplaySystem.MovementVelocity(rb2D, moveSpeed, maxVel);
     }
 
-    //hace lo mismo que el update pero se ejecuta despues de el
+    void Update()
+    {
+        //GameplaySystem.TMovementDelta(transform, moveSpeed);
+    }
+
     void LateUpdate()
     {
         //spr.flipX = FlipSprite;
         IFlip flip = new PlayerFlip();
         spr.flipX = flip.FlipSprite(GameplaySystem.Axis.x, spr);
-
         anim.SetFloat("axisX", Mathf.Abs(GameplaySystem.Axis.x));
-        
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-
         if(other.CompareTag("collectable"))
         {
             Collectable collectable = other.GetComponent<Collectable>();
             Gamemanager.instance.Score.AddPoints(collectable.Points);
-            //score.AddPoints(collectable.Points);
             Destroy(other.gameObject);
         }
-
     }
 
 
